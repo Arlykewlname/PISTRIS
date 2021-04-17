d748ba56e9f6f1c4d8107c2b2d4f09442e293e71
Rushil <rushilp002@gmail.com>
2021-04-17 13:32:33 -0700

    hi

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # This is the main file
 import pygame
+import os
 
-# Screen Variables
+#Screen Variables
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("PISTRIS")
+pygame.display.set_caption("PISTIS")
 
-SCREEN_COLOR = (255, 255, 255)  # White
-FPS = 60  # Frames Per Second
+SCREEN_COLOR = (255,255,255) #White
+FPS = 60 #Frames Per Second
 
+SHARK_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
 
 def draw_window():
     WIN.fill(SCREEN_COLOR)
+
     pygame.display.update()
 
 
@@ -23,9 +27,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
-    pygame.quit()
+    draw_window()
 
+    keys_pressed = pygame.key.get_pressed()
+    (keys_pressed)
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
